c793a6737be29520a30f37e7a5572559ae9af951
lgribeirolg <l.gustavoribeiro@gmail.com>
2025-03-20 22:55:43 +0000

    add currency on head

--- data_science/views.py
+++ data_science/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-        return render(request,'data_science/index.html')
+        dados = cotacao()
+        return render(request,'data_science/index.html',{'dados':dados})
 
 def imagem(request):
+        
         return render(request, 'data_science/imagem.html')
+
+def cotacao():
+        
+
+        # URL do endpoint da CoinGecko para obter preços simples das criptomoedas
+        url = 'https://api.coingecko.com/api/v3/simple/price'
+
+        # Definindo as moedas que você quer buscar
+        params = {
+        'ids': 'bitcoin,ethereum,ripple',  # Moedas que você quer obter preço
+        'vs_currencies': 'eur',  # Moeda para comparação (exemplo: USD)
+        }
+
+        # Fazendo a requisição para a API
+        response = requests.get(url, params=params)
+
+        # Verificando se a requisição foi bem sucedida (status code 200)
+        if response.status_code == 200:
+                data = response.json()
+                # Exibindo os preços das criptomoedas em USD
+                #print(f"Bitcoin Price: ${data['bitcoin']['usd']}")
+                #print(f"Ethereum Price: ${data['ethereum']['usd']}")
+                #print(f"Ripple Price: ${data['ripple']['usd']}")
+        else:
+                erro = (f"Error: {response.status_code}")
+
+        return data
